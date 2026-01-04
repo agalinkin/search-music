3ba80b10dad7c21ee47db31b0a705b3ebb2a1c74
sguitarist <agalinkin@gmail.com>
2026-01-04 15:08:18 +0200

    fix

--- src/app/store/search/search.actions.ts
+++ src/app/store/search/search.actions.ts
@@ -1,5 +1,5 @@
 // store/search/search.actions.ts
-import { Disc } from '../../models/disc.interface';
+//import { Disc } from '../../models/disc.interface';
 
 export class PerformSearch {
   static readonly type = '[Search] Perform Search';
